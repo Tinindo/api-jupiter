48ebb2d06f1b5341aebc00abbbfed65f232928ee
Gabriel Moura <gabriel.brito@linkapi.com.br>
2020-10-25 14:59:57 -0300

    build: mover modulos de propriedades para dentro de useCases

---
+++ src/useCases/Properties/CreateUserProperty/CreateUserPropertyDTO.ts
@@ -0,0 +1,15 @@
+export interface ICreateUserPropertyRequest {
+    living_rooms_quantity: number;
+    bathrooms_quantity: number;
+    kitchens_quantity: number;
+    garages_quantity: number;
+    rooms_quantity: number;
+    postal_code: string;
+    number: string;
+    street: string;
+    complement: string;
+    neighborhood: string;
+    city: string;
+    state: string;
+    country: string;
+}
