90bf3b4cc67fda49ba3314c36c63330c16c503ec
Gabriel Moura <gabriel.brito@linkapi.com.br>
2020-10-26 22:13:13 -0300

    build: criar build JS de produção

--- dist/src/app.js
+++ dist/src/app.js
@@ -5,12 +5,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.app = void 0;
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 require("express-async-errors");
 const errorHandler_1 = __importDefault(require("@middlewares/errorHandler"));
 const index_1 = require("./routes/index");
 require("@database/connection");
 const app = express_1.default();
 exports.app = app;
+app.use(cors_1.default());
 app.use(express_1.default.json());
 app.use(index_1.router);
 app.use(errorHandler_1.default);
